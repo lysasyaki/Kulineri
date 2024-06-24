e32c26abf77b3f7da9f4347c79afb580e29647d9
Alysa Syakirah <alysasyakirah0405@gmail.com>
2024-06-24 18:13:17 +0700

    feat: add ui confirmation logout

---
+++ app/src/main/java/com/alysa/myrecipe/auth/presentation/ConfirmationActivity.kt
@@ -0,0 +1,12 @@
+package com.alysa.myrecipe.auth.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alysa.myrecipe.R
+
+class ConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmation)
+    }
+}
